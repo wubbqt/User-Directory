0272804f085871079af25ef40df9e710db56cb32
Michael <michael.zaccaria1@gmail.com>
2019-11-26 14:24:31 -0800

    Updates

---
+++ precache-manifest.7da36377c077753ec64ccf06c2b8e855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defd81e6b673f9ba895e20a415d74316",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "3e33143a8e9f2bfd5974",
+    "url": "/User-Directory/static/css/main.422edb60.chunk.css"
+  },
+  {
+    "revision": "82b7557759b51cb27cf8",
+    "url": "/User-Directory/static/js/2.bfbb5337.chunk.js"
+  },
+  {
+    "revision": "3e33143a8e9f2bfd5974",
+    "url": "/User-Directory/static/js/main.e2140af0.chunk.js"
+  },
+  {
+    "revision": "1a48e735230d9091ff90",
+    "url": "/User-Directory/static/js/runtime~main.2facd29f.js"
+  }
+]);
